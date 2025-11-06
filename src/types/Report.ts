fc95a1eb28a48a1cc84d640a442accf79642f859
potato <65760583+juntae6942@users.noreply.github.com>
2025-11-06 13:02:19 +0900

    feat: 신고 타입 정의

--- src/types/Report.ts
+++ src/types/Report.ts
@@ -1,8 +1,10 @@
 export interface Report {
-    id: number;
-    title: string;
-    item: string;
-    room: string;
-    createdAt: string;
+    item: string;          // 고장 물품
+    roomNumber: string;    // 강의실 호수
+    userRole: 'student' | 'staff' | ''; // 학생/교직원
+    userId: string;        // 학번/교직원번호
+    contact: string;       // 연락처
+    description: string;   // 고장 상세 기재
+    file?: FileList;       // 첨부 파일 (optional)
     status: 'pending' | 'completed'
 }
