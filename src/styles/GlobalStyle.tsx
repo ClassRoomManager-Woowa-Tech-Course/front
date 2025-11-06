5dc3c6dca1f27e26f18e0a07376c6d785cc21d3d
potato <65760583+juntae6942@users.noreply.github.com>
2025-11-06 13:00:24 +0900

    refactor: 서비스 전체 배경 색깔 지정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,14 +2,13 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+      margin: 0;
+      padding: 0;
   }
   html, body, #root {
-    width: 100%;
-    height: 100%;
-    background-color: #4B48ED;
+      width: 100%;
+      background-color: #4B48ED;
+      min-height: 100vh;
   }
 `;
 
