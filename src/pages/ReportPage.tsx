9761b46fd396c00aad3a5e874340dd1c67dcbe53
potato <65760583+juntae6942@users.noreply.github.com>
2025-11-06 13:02:28 +0900

    feat: 신고 페이지 생성

---
+++ src/pages/ReportPage.tsx
@@ -0,0 +1,80 @@
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import PageLayout from "@/layouts/PageLayout";
+import PageHeader from "@/components/PageHeader";
+import type { Report } from "@/types/Report"
+import {
+    FormGroup, FormLabel, FormRow,
+    StyledInput, StyledSelect, StyledTextArea, StyledButton
+} from '@/components/FormElements';
+import {ButtonWrapper, StyledForm} from "../components/FormElements";
+
+const ReportPage = () => {
+    const { register, handleSubmit } = useForm<Report>();
+
+    const onSubmit: SubmitHandler<Report> = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <PageLayout>
+            <PageHeader
+                title="고장 신고하기"
+                description="수업중 있었던 불편 사항을 기재해주세요."
+            />
+            <StyledForm onSubmit={handleSubmit(onSubmit)}>
+                <h3>신고 내용 입력</h3>
+
+                <FormGroup>
+                    <FormLabel htmlFor="item">고장 물품</FormLabel>
+                    <StyledSelect id="item" {...register('item', { required: true })}>
+                        <option value="">선택하세요</option>
+                        <option value="projector">빔프로젝터</option>
+                        <option value="pc">컴퓨터</option>
+                        <option value="speaker">스피커</option>
+                        <option value="microphone">마이크</option>
+                    </StyledSelect>
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel htmlFor="roomNumber">강의실 호수</FormLabel>
+                    <StyledInput id="roomNumber" type="text" {...register('roomNumber', { required: true })}/>
+                </FormGroup>
+
+                <FormRow style={{ gridTemplateColumns: '1fr 2fr 2fr' }}>
+                    <FormGroup>
+                        <FormLabel htmlFor="userRole">학생/교직원</FormLabel>
+                        <StyledSelect id="userRole" {...register('userRole', { required: true })}>
+                            <option value="">선택하세요</option>
+                            <option value="student">학생</option>
+                            <option value="staff">교직원</option>
+                        </StyledSelect>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <FormLabel htmlFor="userId">학번/교직원번호</FormLabel>
+                        <StyledInput id="userId" type="text" {...register('userId', { required: true })} />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <FormLabel htmlFor="contact">연락처</FormLabel>
+                        <StyledInput id="contact" type="tel" {...register('contact', { required: true })} />
+                    </FormGroup>
+                </FormRow>
+
+                <FormGroup>
+                    <FormLabel htmlFor="description">고장 상세 기재</FormLabel>
+                    <StyledTextArea id="description" {...register('description', { required: true })} />
+                </FormGroup>
+
+                <FormGroup>
+                    <FormLabel htmlFor="file">첨부 파일</FormLabel>
+                    {/* '첨부 파일' 영역은 StyledInput을 재활용하거나 커스텀 FileDropzone을 만듭니다. */}
+                    <StyledInput id="file" type="file" {...register('file')} />
+                </FormGroup>
+                <ButtonWrapper>
+                    <StyledButton type="submit">신고하기</StyledButton>
+                </ButtonWrapper>
+            </StyledForm>
+        </PageLayout>
+    )
+}
+export default ReportPage;
