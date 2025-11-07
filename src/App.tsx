865fd7f94f6d1f61d00f99c7c7d1cb8616155954
potato <65760583+juntae6942@users.noreply.github.com>
2025-11-07 09:23:03 +0900

    feat: 강의실 예약 현황 조회 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ReportPage from '@/pages/ReportPage';
-import MainPage from "./pages/MainPage";
-import GlobalStyle from "./styles/GlobalStyle";
-import ReservationPage from "./pages/ReservationPage";
+import MainPage from "@/pages/MainPage";
+import GlobalStyle from "@/styles/GlobalStyle";
+import ReservationPage from "@/pages/ReservationPage";
+import ReservationStatusPage from "@/pages/ReservationStatusPage";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<MainPage />}/>
             <Route path="/report" element={<ReportPage />}/>
             <Route path="/reservation" element={<ReservationPage />}/>
+            <Route path="/status" element={<ReservationStatusPage />}/>
         </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/ReservationStatusPage.tsx
@@ -0,0 +1,18 @@
+import ListPageLayout from "@/layouts/ListPageLayout";
+import ListPageHeader from "@/components/ListPageHeader";
+
+const ReservationStatusPage = () => {
+
+    return (
+        <ListPageLayout>
+            <ListPageHeader
+                title="강의실 예약 현황"
+                description="강의실 예약 현황 페이지입니다. 예약 후 학과 사무실에 연락주세요."
+            />
+
+        </ListPageLayout>
+    )
+
+}
+
+export default ReservationStatusPage;
