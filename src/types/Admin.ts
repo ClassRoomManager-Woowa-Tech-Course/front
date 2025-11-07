81b348c6801fe433d1a240bbd1349c28e1ee1fc6
juntae6942 <juntae6942@o.cnu.ac.kr>
2025-11-07 18:27:40 +0900

    refactor: 관리자 필드 추가

--- src/types/Admin.ts
+++ src/types/Admin.ts
@@ -1,9 +1,12 @@
-import type {Role} from "@/types/Role";
+import type {Authorization} from "./Authorization";
 
 export interface Admin {
     id: number;
     userId: number;
     name: string;
-    phoneNumber: string;
-    role: Role;
+    contact: string;
+    password: string;
+    userRole:string;
+    Authorization: Authorization;
+    status:'ACTIVE'|'INACTIVE';
 }
